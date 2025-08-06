166b2330341cb877e2392c7c24f047d9844b143c
Jakub Mroz <kuba.mroz01@gmail.com>
2025-08-06 14:29:14 +0200

    feat: core implemented

--- exercise.main/Core.cs
+++ exercise.main/Core.cs
@@ -19,14 +19,58 @@ namespace exercise.main
         public bool winningPair(IEnumerable<Tuple<string, string>> hands, out Tuple<string, string> result)
         {
             result = new Tuple<string,string>(string.Empty, string.Empty);
+            int highestWinnerPairValue = 0;
 
-            // your code here...          
+            // your code here...
+
+            foreach (var pair in hands)
+            {
+                var score = GetValueOfCard(pair.Item1) + GetValueOfCard(pair.Item2);
+
+                if (pair.Item1 == pair.Item2 && score > highestWinnerPairValue)
+                {
+                    result = new Tuple<string, string>(pair.Item1, pair.Item2);
+                    highestWinnerPairValue = score;
+                }
+            }
 
             return result.Item1!=string.Empty ? true : false;
         }
         public int GetValueOfCard(string card)
         {
-            return  0;           
+            switch (card) 
+            {
+                case "1":
+                    return 1;
+                case "2":
+                    return 2;
+                case "3":
+                    return 3;
+                case "4":
+                    return 4;
+                case "5":
+                    return 5;
+                case "6":
+                    return 6;
+                case "7":
+                    return 7;
+                case "8":
+                    return 8;
+                case "9":
+                    return 9;
+                case "10":
+                    return 10;
+                case "J":
+                    return 11;
+                case "Q":
+                    return 12;
+                case "K":
+                    return 13;
+                case "A":
+                    return 14;
+                default:
+                    return 0;
+            }           
         }
     }
 }
